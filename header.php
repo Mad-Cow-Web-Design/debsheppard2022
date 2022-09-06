e6d0e9e058ee34d34740adaa60b870979094bb55
Jason Robie <jason@madcowweb.com>
2022-09-06 11:59:13 -0600

    styling gtb page mostly youtube buttons

--- header.php
+++ header.php
@@ -42,6 +42,25 @@ src="https://www.facebook.com/tr?id=1639257369453041&ev=PageView
 <meta name="facebook-domain-verification" content="monb5fl0h7b4gpvl3i261q588uwyoh" />
 <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet">
 	<?php wp_head(); ?>
+    <meta name="facebook-domain-verification" content="900dn288qswhhchlsp66k1svg53nld" />
+
+    <!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1021325791862908');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1021325791862908&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
 </head>
 
 <body <?php body_class(); ?>>
